cb1757c5429cd4ef3f751467aa01d7c2d386fe12
卓羿呈 <84216267+Arden-Cho@users.noreply.github.com>
2024-11-13 20:49:37 +0800

    refactor code

--- ident.py
+++ ident.py
@@ -1,44 +1,56 @@
 #! /usr/bin/env python3
 from sys import argv, exit
-from os.path import isfile
-from hashlib import sha3_256 as sig
+from os.path import isfile, isdir
+from hashlib import sha3_256 as get_hash
 argv.pop(0)
 
-def help(arg_error: bool = False):
-    if arg_error: print("\033[91mIncorrect amount of arguments!\nExpected 2, got", str(len(argv)) + ".\x1b[0m\n")
+def colored(color: str, msg: str) -> str:
+    colors = {
+        "FAIL": "\033[91m",
+        "OKGREEN": "\033[92m",
+        "WARNING": "\033[93m"
+    }
+    try:
+        return colors[color] + msg + "\033[0m"
+    except KeyError:
+        return msg
+
+def print_help(arg_error: bool = False):
+    if arg_error: print(colored("FAIL", f"Incorrect amount of arguments!\nExpected 2, got {len(argv)}."))
     print("Usage: python3 ident.py [file1] [file2]")
     exit(arg_error)
 
 if len(argv) == 0:
-    help()
+    print_help()
 
 if len(argv) != 2:
-    help(True)
+    print_help(True)
 
 file1_name = argv[0]
 file2_name = argv[1]
 
-if not isfile(file1_name):
-    print("\033[91mFile", file1_name, "doesn't exist or is a directory.\x1b[0m")
-    exit(1)
+if not isfile(file1_name) or not isfile(file2_name):
+    if not isfile(file1_name):
+        if isdir(file1_name): print(colored("FAIL", f"\"{file1_name}\" is a directory."))
+        else: print(colored("FAIL", f"\"{file1_name}\" does not exist."))
 
-if not isfile(file2_name):
-    print("\033[91mFile", file2_name, "doesn't exist or is a directory.\x1b[0m")
+    if not isfile(file2_name):
+        if isdir(file2_name): print(colored("FAIL", f"\"{file2_name}\" is a directory."))
+        else: print(colored("FAIL", f"\"{file2_name}\" does not exist."))
     exit(1)
 
 try:
     with open(file1_name, "rb") as file1, open(file2_name, "rb") as file2:
-        file1_hash = sig(file1.read()).hexdigest()
-        file2_hash = sig(file2.read()).hexdigest()
+        file1_hash = get_hash(file1.read()).hexdigest()
+        file2_hash = get_hash(file2.read()).hexdigest()
 except Exception as e:
-    print("\033[91mError!\n" + str(e) + "\x1b[0m")
+    print(colored("FAIL", f"Error while reading file.\n{str(e)}"))
     exit(1)
-
 
 print(file1_name + ":", file1_hash[:18] + "...")
 print(file2_name + ":", file2_hash[:18] + "...")
 
 if file1_hash == file2_hash:
-    print("\033[92mIdentical.\033[0m")
+    print(colored("OKGREEN", "Identical."))
 else:
-    print("\033[93mNot identical.\033[0m")
+    print(colored("WARNING", "Not identical."))
